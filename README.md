d8741ecfc5bb57bd337d3ff2d530d4c828ff6b85
Guangcong Luo <guangcongluo@gmail.com>
2012-09-28 21:07:10 -0500

    Corrected version: - resolves several mathematical errors in implementation - updates for PHP5 compatibility

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 glicko2-php
 ===========
 
-A PHP implementation of Glicko2
+A PHP implementation of [Glicko2][1], a rating system for game ladders.
+Glicko2 is a refinement of the well-known [Elo][2] rating system that adds
+the concepts of rating deviation, volatility, and rating decay.
+
+The original glicko2phplib was written by Noah Smith on 2011 June 7. That
+version contains significant mathematical errors. This is a fork by Guangcong
+Luo on 2012 Sept 28 that corrects these errors, and also updates it to be
+compatible with PHP 5. It should also work on PHP 4, although I have not
+tested that.
+
+ [1]: http://en.wikipedia.org/wiki/Glicko_rating_system
+ [2]: http://en.wikipedia.org/wiki/Elo_rating_system
+
+Usage
+-----
+
+	Glicko2Player([$rating = 1500 [, $rd = 350 [, $volatility = 0.06 [, $mu [, $phi [, $sigma [, $systemconstant = 0.75 ]]]]]]])
+
+For new players, use the default values for `rating`, `rd`, and `volatility`.
+
+The `systemconstant` should be between 0.3 and 1.2, depending on system itself
+(this is game dependent, and must be set by estimation or experimentation)
+
+Updating a Glicko2Player
+------------------------
+
+Add wins, losses, and draws to a player:
+
+	$Alice = new Glicko2Player();
+	$Bob = new Glicko2Player();
+	$Charlie = new Glicko2Player();
+
+	$Alice->AddWin($Bob);
+	$Alice->AddWin($Charlie)
+
+	$Bob->AddLoss($Alice);
+	$Bob->AddWin($Charlie);
+
+	$Charlie->AddLoss($Alice);
+	$Charlie->AddLoss($Bob);
+
+	$Alice->Update();
+	$Bob->Update();
+	$Charlie->Update();
--- class.Glicko2Player.php
+++ class.Glicko2Player.php
@@ -2,6 +2,18 @@
 
 /*******************************************************************************
 
+Corrected implementation of Glicko2
+
+Modified by Guangcong Luo on 2012 Sept 28
+<guangcongluo@gmail.com>
+
+Modified from Noah Smith's implementation, which contains a number of mathematical
+errors.
+
+His original documentation is reproduced below.
+
+*******************************************************************************
+
 glicko-2 ranking system
 
 Written by Noah Smith 2011, June 7
@@ -47,62 +59,57 @@ class Glicko2Player {
 	public $rating;
 	public $rd;
 	public $sigma;
-	
+
 	public $mu;
 	public $phi;
 	public $tau;
-	
-	private $pi2;
-	
-	private $M;
+
+	private $pi2 = 9.8696044;
+
+	var $M = array();
 
 	function __construct($rating = 1500, $rd = 350, $volatility = 0.06, $mu = null, $phi = null, $sigma = null, $systemconstant = 0.75) {
-		$pi2  = pi() * pi();
-		$M = array();
 		// Step 1
 		$this->rating = $rating;
 		$this->rd = $rd;
 		// volatility
-		if (isnull($sigma)) {
+		if (is_null($sigma)) {
 			$this->sigma = $volatility;
 		} else {
 			$this->sigma = $sigma;
 		}
 		// System Constant
 		$this->tau = $systemconstant;
-		
+
 		// Step 2
 		// Rating
-		if (isnull($mu)) {
+		if (is_null($mu)) {
 			$this->mu = ( $this->rating - 1500 ) / 173.7178;
 		} else {
 			$this->mu = $mu;
 		}
 		// Rating Deviation
-		if (isnull($phi)) {
+		if (is_null($phi)) {
 			$this->phi = $this->rd / 173.7178;
 		} else {
 			$this->phi = $phi;
 		}
 	}
-	
+
 	function AddWin($OtherPlayer) {
-		$M[] = $OtherPlayer->MatchElement(1);
+		$this->M[] = $OtherPlayer->MatchElement(1);
 	}
-	
+
 	function AddLoss($OtherPlayer) {
-		$M[] = $OtherPlayer->MatchElement(0);
+		$this->M[] = $OtherPlayer->MatchElement(0);
 	}
-	
+
 	function AddDraw($OtherPlayer) {
-		$M[] = $OtherPlayer->MatchElement(0.5);
+		$this->M[] = $OtherPlayer->MatchElement(0.5);
 	}
-	
-	function Update($M) {
-		if (isnull($M)) {
-			$M = $this->M;
-		}
-		$Results = $this->AddMatches($M);
+
+	function Update() {
+		$Results = $this->AddMatches($this->M);
 		$this->rating = $Results['r'];
 		$this->rd = $Results['RD'];
 		$this->mu = $Results['mu'];
@@ -110,77 +117,79 @@ class Glicko2Player {
 		$this->sigma = $Results['sigma'];
 		$this->M = array();
 	}
-	
+
 	function MatchElement($score) {
 		return array( 'mu' => $this->mu, 'phi' => $this->phi, 'score' => $score );
 	}
-	
+
 	function AddMatches($M) {
-		if (isnull($M)) {
-			$M = $this->M;
-		}
+		// This is where the Glicko2 rating calculation actually happens
+
+		// Follow along the steps using: http://www.glicko.net/glicko/glicko2.pdf
+
 		if (count($M) == 0) {
 			$phi_p = sqrt( ( $this->phi * $this->phi ) + ( $this->sigma + $this->sigma ) );
 			return array( 'r' => $this->rating, 'RD' => 173.7178 * $phi_p, 'mu' => $this->mu, 'phi' => $phi_p, 'sigma' => $this->sigma ) ;
 		}
-		
-		// Step 3 & 4 & 7
-		// Estimated variance
-		$v = 0;
-		// Estimated improvment in rating
-		$delta = 0;
-		// New mu
-		$mu_p = 0;
+
+		// summation parts of Step 3 & 4 & 7
+		$v_sum = 0;
+		$delta_sum = 0;
+		$mu_p_sum = 0;
 		for ($j = 0; $j < count($M); $j++) {
-			$E = $this->E( $this->mu, $m[$j]['mu'], $m[$j]['phi'] );
+			$E = $this->E( $this->mu, $M[$j]['mu'], $M[$j]['phi'] );
 			$g = $this->g( $M[$j]['phi'] );
-			$v +=  1.0 / ( $g * $g * $E * ( 1 - $E ) );
-			
-			$delta += $g * ( $M['score'] - $E );
-			
-			$mu_p += $g * ( $M[$j]['score'] - $E );
+			$v_sum +=  ( $g * $g * $E * ( 1 - $E ) );
+
+			$delta_sum += $g * ( $M[$j]['score'] - $E );
+
+			$mu_p_sum += $g * ( $M[$j]['score'] - $E );
 		}
-		
-		// Step 4 (finalize)
-		$delta *= $v;
-		
+
+		// Step 3
+		// Estimated variance
+		$v = 1.0 / $v_sum;
+
+		// Step 4
+		// Estimated improvment in rating
+		$delta = $v * $delta_sum;
+
 		// Step 5
 		$a = log( $this->sigma * $this->sigma );
 		$x_prev = $a;
 		$x = $x_prev;
 		$tausq = $this->tau * $this->tau;
-		$phsq = $this->phi * $this->phi;
+		$phisq = $this->phi * $this->phi;
 		$deltasq = $delta * $delta;
 		do {
 			$exp_xp = exp( $x_prev );
-			$d = $this->phi * $this->phi + $exp_xp;
-			$deltadsq = ( $delta / $d ) * ( $delta / $d );
-			$h1 = -( $x_prev - $a ) / ( $tausq ) - ( 0.5 * $exp_xp / $d ) + ( 0.5 * $exp_xp * $deltadq );
+			$d = $this->phi * $this->phi + $v + $exp_xp;
+			$deltadsq = $deltasq / ($d * $d);
+			$h1 = -( $x_prev - $a ) / ( $tausq ) - ( 0.5 * $exp_xp / $d ) + ( 0.5 * $exp_xp * $deltadsq );
 			$h2 = ( -1.0 / $tausq ) - ( ( 0.5 * $exp_xp ) * ( $phisq + $v ) / ( $d * $d ) ) + ( 0.5 * $deltasq * $exp_xp * ( $phisq + $v - $exp_xp ) / ( $d * $d * $d ) );
 			$tmp_x = $x;
 			$x = $x_prev - ( $h1 / $h2 );
 			$x_prev = $tmp_x;
 		} while (abs($x - $x_prev) > 0.1);
-		
+
 		$sigma_p = exp( $x / 2 );
-		
+
 		// Step 6
-		$phi_star = sqrt( $phsq + ( $sigma_p * $sigma_p ) );
-		
+		$phi_star = sqrt( $phisq + ( $sigma_p * $sigma_p ) );
+
 		// Step 7
 		$phi_p = 1.0 / ( sqrt( ( 1.0 / ( $phi_star * $phi_star ) ) + ( 1.0 / $v ) ) );
-		$mu_p = $mu + $phi_p * $phi_p * $mu_p;
-		
-		return array( 'r' => ( 173.7178 * $mu_p ) + 1500, 'RD' => 173.7178 * $phi_p, 'mu' => $mu_p, 'phi' => $phi_p, 'sigma' => $sigma_p ) ;
+		// New mu
+		$mu_p = $this->mu + $phi_p * $phi_p * $mu_p_sum;
+
+		return array( 'r' => ( 173.7178 * $mu_p ) + 1500, 'RD' => 173.7178 * $phi_p, 'mu' => $mu_p, 'phi' => $phi_p, 'sigma' => $sigma_p );
 	}
-	
+
 	function g($phi) {
 		return 1.0 / ( sqrt( 1.0 + ( 3.0 * $phi * $phi) / ( $this->pi2 ) ) );
 	}
-	
+
 	function E($mu, $mu_j, $phi_j) {
 		return 1.0 / ( 1.0 + exp( -$this->g($phi_j) * ( $mu - $mu_j ) ) );
 	}
 }
-
-?>
